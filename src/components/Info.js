cb92786ec0499869aed43d4975d8fb9da24be32e
KaranRandhir <karanrandhir123@gmail.com>
2020-04-27 05:41:39 +0530

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,7 +4,7 @@ import "./Info.css"
 const Info = ({heading,data,image})=>{
     return(
         <div className="info-block">
-        <div>{heading}</div>
+        <div><strong>{heading}</strong></div>
         <div><img alt="error"src={image} style={{height:"8vh",width:"8vh"}}/></div>
         <div>{data}</div>
         </div>
